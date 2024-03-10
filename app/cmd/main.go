42638a0c27e5840c467e22db292b32fb5c37d676
MateoG404 <mgutierrezca@unal.edu.co>
2024-03-10 08:39:01 -0500

    feat(main): Created config and code to run the server 🥳

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"ms_music/app/internal/application"
+)
 
 func main() {
-	fmt.Println("Hello, world! ")
+	fmt.Println("Initializing server ...")
+
+	// env
+	// ...
+
+	// app
+	// -config
+
+	// Empty config for now to use the default config
+	cfg := &application.ConfigServerChi{
+		Addr: ":8080",
+		Port: 8080,
+	}
+	server := application.NewServerChi(cfg)
+	err := server.Run()
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+
 }
