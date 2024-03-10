a651538989647ccf8a12f922a8c5968b42a3ccc4
MateoG404 <mgutierrezca@unal.edu.co>
2024-03-09 20:08:09 -0500

    feat(tracks): New interface for the service and the repository to the Track

--- app/internal/track.go
+++ app/internal/track.go
@@ -18,3 +18,49 @@ type Track struct {
 	TrackNumber      int      // The number of the track. If an album has several discs, the track number is the number on the specified disc.
 
 }
+
+// TrackRepository is the interface that provides track methods
+type TrackRepository interface {
+	// Get Methods
+	GetTrack(id string) (Track, error)                                     // Get a track
+	GetTrackByArtist(artist string) ([]Track, error)                       // Get tracks by artist
+	GetTrackByAlbum(album string) ([]Track, error)                         // Get tracks by album
+	GetTrackByGenre(genre string) ([]Track, error)                         // Get tracks by genre
+	GetTrackByPopularity(popularity int) ([]Track, error)                  // Get tracks by popularity
+	GetTrackByDuration(duration int) ([]Track, error)                      // Get tracks by duration
+	GetTrackByReleaseDate(releaseDate string) ([]Track, error)             // Get tracks by release date
+	GetTrackBetweenDuration(duration1 int, duration2 int) ([]Track, error) // Get tracks between duration
+	GetTrackAvailableInMarket(market string) ([]Track, error)              // Get tracks available in market
+
+	// Create Methods
+	CreateTrack(track Track) (Track, error)
+
+	// Update Methods
+	UpdateTrack(track Track) (Track, error)
+
+	// Delete Methods
+	DeleteTrack(id string) error
+}
+
+// TrackService is the interface that provides track methods
+type TrackService interface {
+	// Get Methods
+	GetTrack(id string) (Track, error)                                     // Get a track
+	GetTrackByArtist(artist string) ([]Track, error)                       // Get tracks by artist
+	GetTrackByAlbum(album string) ([]Track, error)                         // Get tracks by album
+	GetTrackByGenre(genre string) ([]Track, error)                         // Get tracks by genre
+	GetTrackByPopularity(popularity int) ([]Track, error)                  // Get tracks by popularity
+	GetTrackByDuration(duration int) ([]Track, error)                      // Get tracks by duration
+	GetTrackByReleaseDate(releaseDate string) ([]Track, error)             // Get tracks by release date
+	GetTrackBetweenDuration(duration1 int, duration2 int) ([]Track, error) // Get tracks between duration
+	GetTrackAvailableInMarket(market string) ([]Track, error)              // Get tracks available in market
+
+	// Create Methods
+	CreateTrack(track Track) (Track, error)
+
+	// Update Methods
+	UpdateTrack(track Track) (Track, error)
+
+	// Delete Methods
+	DeleteTrack(id string) error
+}
