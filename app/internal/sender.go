3d30c6f82cd94974b03c0557f3b7de829720735d
Lanna-27 <lalacast2000@gmail.com>
2024-03-31 14:05:26 -0500

    update sender

--- app/internal/sender.go
+++ app/internal/sender.go
@@ -1,4 +1,4 @@
-package main
+package internal
 
 import (
 	"context"
@@ -14,7 +14,7 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func main() {
+func send() {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
